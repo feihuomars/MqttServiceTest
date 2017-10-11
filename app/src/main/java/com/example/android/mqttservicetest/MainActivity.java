e6c76bcc985bc7794eeee238ad98e614c45a5ae5
feihuomars <feihuomars@outlook.com>
2017-10-11 09:35:17 +0800

    增加了滑动菜单

--- app/src/main/java/com/example/android/mqttservicetest/MainActivity.java
+++ app/src/main/java/com/example/android/mqttservicetest/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -37,7 +40,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
+    private DrawerLayout drawerLayout;
     public FragmentTabHost fragmentTabHost;
     FragmentManager fragmentManager;
 
@@ -93,6 +96,13 @@ public class MainActivity extends AppCompatActivity {
         //设置toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.main_drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
 
         fragmentTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         fragmentTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
@@ -332,6 +342,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
             case R.id.backup:
                 Toast.makeText(this, "Backup Clicked", Toast.LENGTH_SHORT).show();
                 break;
